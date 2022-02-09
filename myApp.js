ef93d22a27f9906004b8244bb558b9b528c8be36
ckroot <39632390+saliouseck2009@users.noreply.github.com>
2022-02-09 20:43:39 +0000

    Send the /views/index.html file as a response to GET requests to the / path.

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+// app.get("/", function(req, res){
+//   res.send("Hello Express");
+// });
+absolutePath = __dirname + "/views/index.html";
 app.get("/", function(req, res){
-  res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
-
 
 
 
